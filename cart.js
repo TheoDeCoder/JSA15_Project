7b7010b4ea9fa1847498b87a74857bcdbb32d816
NgoDinhNguyenPhuc <ngodinhnguyenphuc@gmail.com>
2022-03-20 18:46:51 +0700

    dem qua em tuyet lam

--- cart.js
+++ cart.js
@@ -1,21 +1,22 @@
 import data from "./data.js";
 
-let table = document.querySelector("#table")
-let cart = localStorage.getItem("cart")
+let table = document.querySelector("#table");
+let cart = localStorage.getItem("cart");
 let totalprice = 0;
-cart = JSON.parse(cart)
-cart.forEach(item => {
-    table.innerHTML +=
-        ` <tr>
+cart = JSON.parse(cart);
+cart.forEach((item) => {
+  table.innerHTML += ` <tr>
         <td class="td_img"><img src="${item.image_url}"></td>
         <td class="td_name">${item.name}</td>
         <td class="td_stock">1</td>
         <td class="td_price">${item.price.VND} VND</td>
     </tr>
-      `
-    totalprice += item.price.VND
-})
-table.innerHTML += 
-`<tr> 
+      `;
+  totalprice += item.price.VND;
+});
+table.innerHTML += `<tr class="totalprice"> 
     <td colspan="3"> Tổng tiền: </td> <td> ${totalprice} VND</td>
-</tr>`
+</tr>`;
+function clearCart() {
+  localStorage.clear();
+}
